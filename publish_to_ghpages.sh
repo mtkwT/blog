9c915c9ff4b053be1d9fbaf3f4ed42c5bf8db1a5
mtkwT <moto.t.03.td@gmail.com>
2021-01-09 23:35:21 +0900

    update

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -1,11 +1,11 @@
 
 #!/bin/sh
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+# if [ "`git status -s`" ]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+#     exit 1;
+# fi
 
 echo "Deleting old publication"
 rm -rf public
@@ -25,5 +25,6 @@ hugo
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
+git push upstream gh-pages
 #echo "Pushing to github"
 #git push --all
